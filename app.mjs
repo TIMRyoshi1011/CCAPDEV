6895c04b49eea9089e47abae813160c6c8ce0bd8
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2026-03-10 02:41:50 +0800

    Updated app.mjs for MongoDB Connection

--- app.mjs
+++ app.mjs
@@ -1,9 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import path from "path";
+import 'dotenv/config';
+import { connectToMongo, getDb } from "./db/conn.js";
 
 const app = express();
-
 const port = 3000;
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static('public')); // launches the html files
 
@@ -11,7 +16,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('public/Landing.html')); // make Landing.html default
 });
 
+// for MongoDB Connection
+connectToMongo((err) => {
+    if(err) {
+        console.log("error occurred");
+        console.error(err);
+        process.exit();
+    }
+    console.log("Connected to MongoDB server");
+});
+
 // server activation
 app.listen(port, () => {
     console.log(`Server runinng at http://localhost:${port}`);
+
 });
